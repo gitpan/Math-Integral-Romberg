cc7aa6650c361a72fc8ebe5382904819dfccd087
Eric Boesch <ericboesch@gmail.com>
2008-10-15 16:41:29 +0000

    Import of BOESCH/Math-Integral-Romberg-0.03 from CPAN.
    gitpan-cpan-distribution: Math-Integral-Romberg
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BOESCH/Math-Integral-Romberg-0.03.tar.gz
    gitpan-cpan-author:       BOESCH
    gitpan-cpan-maturity:     released

--- lib/Math/Integral/Romberg.pm
+++ lib/Math/Integral/Romberg.pm
@@ -15,7 +15,7 @@ use vars qw( @ISA @EXPORT @EXPORT_OK );
 use vars qw( $VERSION $abort $return_point_count
 	     $rel_err $abs_err $max_split $min_split );
 
-$VERSION = "0.02";
+$VERSION = "0.03";
 
 $abort = 0;
 $return_point_count = 0;
@@ -27,11 +27,12 @@ $min_split = 5;
 
 =head1 NAME
 
-  Math::Integral::Romberg - scalar numerical integration
+Math::Integral::Romberg - Scalar numerical integration
 
 =head1 SYNOPSIS
 
     use Math::Integral::Romberg 'integral';
+    sub f { my ($x) = @_; 1/($x ** 2 + 1) } # ... or whatever
     $area = integral(\&f, $x1, $x2);    # Short form
     $area = integral                    # Long form
      (\&f, $x1, $x2, $rel_err, $abs_err, $max_split, $min_split);
@@ -58,34 +59,34 @@ A reference to the function to be integrated.
 
 =item $x1, $x2
 
-The two extreme values of the range to be integrated.  C<&$f(x)> must be
-finite at $x1 and $x2.
+The limits of the integration domain. C<&$f(x1)> and C<&$f(x2)> must
+be finite.
 
 =item $rel_err
 
-Maximum acceptable relative error . Estimates of
-relative and absolute error are based on a comparison of the estimate
-computed using C<2**n + 1> points with the estimate computed using
-C<2**(n-1) + 1> points.
+Maximum acceptable relative error. Estimates of relative and absolute
+error are based on a comparison of the estimate computed using C<2**n
++ 1> points with the estimate computed using C<2**(n-1) + 1>
+points.
 
 Once $min_split has been reached (see below), computation stops as
 soon as relative error drops below $rel_err, absolute error drops
 below $abs_err, or $max_split is reached.
 
 If not supplied, uses the value B<$Math::Integral::Romberg::rel_err>
-whose default is 10**-10. A value of 10**-15 is close to
-the accuracy limits of double-precision floating point.
+whose default is 10**-10. The accuracy limit of double-precision
+floating point is about 10**-15.
 
 =item $abs_err
 
-Maximum acceptable absolute error. If not supplied uses
+Maximum acceptable absolute error. If not supplied, uses
 B<$Math::Integral::Romberg::abs_err>, which defaults to
 10**-20.
 
 =item $max_split
 
 At most C<2 ** $max_split + 1> different sample x values are used to
-estimate the integral of C<f()>. If not supplied, uses the value of
+estimate the integral of C<f()>. If not supplied, uses the value
 B<$Math::Integral::Romberg::max_split>, which defaults to 16,
 corresponding to 65537 sample points.
 
@@ -131,7 +132,7 @@ much difference for badly behaved ones.
 
 =head1 AUTHOR
 
-Eric Boesch (ebo@notes.dannet.dk)
+Eric Boesch (ericboesch@gmail.com)
 
 =cut
 
@@ -140,7 +141,7 @@ sub integral {
   my $abort = \$Math::Integral::Romberg::abort;
   my ($f,$lo,$hi,$rel_err,$abs_err,$max_split,$min_split)=@_;
   ($lo, $hi) = ($hi, $lo) if $lo > $hi;
-  
+
   $rel_err	||= $Math::Integral::Romberg::rel_err;
   $abs_err	||= $Math::Integral::Romberg::abs_err;
   $max_split	||= $Math::Integral::Romberg::max_split;
@@ -149,14 +150,14 @@ sub integral {
   my ($estimate, $split, $steps);
   my $step_len = $hi - $lo;
   my $tot = (&$f($lo) + &$f($hi))/2;
-  
+
   # tot is used to compute the trapezoid approximations.  It is more or
   # less a total of all f() values computed so far.  The trapezoid
   # method assigns half as much weight to f(hi) and f(lo) as it does to
   # all other f() values, so f(hi) and f(lo) are divided by two here.
-  
+
   my @row = $estimate = $tot * $step_len; # 0th trapezoid approximation.
-  
+
   for ($split = 1, $steps=2; ; $split++, $step_len /=2, $steps *= 2) {
     my ($x, $new_estimate);
 
@@ -172,24 +173,24 @@ sub integral {
       $tot += &$f($x);
     }
     unshift @row, $tot * $step_len / 2;
-    
+
     # Compute the more refined approximations, based on the (split)th
     # trapezoid approximation and the various (split-1)th refined
     # approximations stored in @row.
-    
+
     my $pow4 = 4;
-    
+
     foreach my $td ( 1 .. $split ) {
       $row[$td] = $row[$td-1] +
 	($row[$td-1]-$row[$td])/($pow4 - 1);
       $pow4 *= 4;
     }
-    
+
     # row[0] now contains the (split)th trapezoid approximation,
     # row[1] now contains the (split)th Simpson approximation, and
     # so on up to row[split] which contains the (split)th Romberg
     # approximation.
-    
+
     # Is this estimate accurate enough?
     $new_estimate = $row[-1];
     if (($split >= $min_split &&
